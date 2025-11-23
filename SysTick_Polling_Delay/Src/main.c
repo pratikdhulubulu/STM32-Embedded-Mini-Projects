b2c7637a852106fd67d7817c0a4c7b30db5c91af
Pratik <pratikdhulubulupsd3@gmail.com>
2025-11-23 22:18:03 +0530

    Update Comments

--- SysTick_Polling_Delay/Src/main.c
+++ SysTick_Polling_Delay/Src/main.c
@@ -1,38 +1,49 @@
+/**
+ * @file    main.c
+ * @author  Pratik Dhulubulu
+ * @brief   LED toggle using SysTick delay on STM32F446RE.
+ * @details Demonstrates SysTick polling-based delay
+ */
+
 #include "stm32f446xx.h"
 
-void delay(void) {
-    for (volatile int i = 0; i < 1000000; i++);
-}
-
+/**
+ * @brief   Blocking millisecond delay using SysTick.
+ * @details Configures SysTick for a 1 ms tick and generates a blocking delay of ms.
+ * @param   ms  Number of milliseconds to delay.
+ * @retval  None
+ */
 void delay_ms(uint32_t ms)
 {
-    // Step 1: Set reload value for 1ms delay
-    // SysTick is 24-bit timer, counts down from RVR to 0
-    // CPU clock = 50 MHz → 1ms = 50,000 ticks
-    // We subtract 1 because counting includes 0
+    /* Load value for 1 ms: 50 MHz clock → 50,000 ticks */
     SysTick->LOAD = 49999;
+
     SysTick->VAL = 0;
-    SysTick->CTRL = (1<<2) | (1<<0); 
+    SysTick->CTRL = (1 << 2) | (1 << 0);
+
+    /* Wait for 'ms' milliseconds */
     for(uint32_t i = 0; i < ms; i++)
     {
-        while(!(SysTick->CTRL & (1 << 16)));  // Poll COUNTFLAG
+        while(!(SysTick->CTRL & (1 << 16)));
     }
-    SysTick->CTRL = 0;  
+
+    SysTick->CTRL = 0;
 }
 
-int main(void)
-{
-    /* 1) Enable clock for GPIOA */
+/**
+ * @brief   Main program entry point.
+ * @details Enables GPIOA clock and toggles PA5 LED every 1 second.
+ * @param   None
+ * @retval  int (never returns).
+ */
+int main(void){
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    /* 2) Configure PA5 as output
-       MODER5 = 01 (Output mode)
-    */
-    GPIOA->MODER &= ~(3U << (5 * 2));   // clear mode bits
-    GPIOA->MODER |=  (1U << (5 * 2));   // set to output mode
+    GPIOA->MODER &= ~(3U << (5 * 2));
+    GPIOA->MODER |=  (1U << (5 * 2));
 
     while (1) {
-        GPIOA->ODR ^= (1U << 5);        // Toggle PA5
+        GPIOA->ODR ^= (1U << 5);
         delay_ms(1000);
     }
 }
